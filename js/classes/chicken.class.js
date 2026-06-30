9417579e0360b201d496dd26ba085338b08d837d
Jonas Nell <jonasnell1705@gmail.com>
2026-06-30 14:17:34 +0200

    fix: fixed mo positioning

--- js/classes/chicken.class.js
+++ js/classes/chicken.class.js
@@ -2,6 +2,7 @@ import { ImageHelper } from "./imgHelper.class.js";
 import { MovableObject } from "./movableObject.class.js";
 
 export class Chicken extends MovableObject{
+    y = 290;
 
     constructor(){
         super().loadImage(ImageHelper.CHICKEN.walk[0]);
--- js/classes/movableObject.class.js
+++ js/classes/movableObject.class.js
@@ -1,8 +1,8 @@
 export class MovableObject{
     x = 120;
-    y = 250;
+    y = 280;
     img;
-    height = 100;
+    height = 150;
     width = 100;
 
     loadImage(path) {
