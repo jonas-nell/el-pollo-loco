147f8a9db5a6a28ea10958334a23db2e5af2381b
Jonas Nell <jonasnell1705@gmail.com>
2026-07-01 12:19:24 +0200

    feat: animated clouds

--- js/classes/cloud.class.js
+++ js/classes/cloud.class.js
@@ -1,4 +1,5 @@
 import { ImageHelper } from "./imgHelper.class.js";
+import { IntervalHub } from "./interal-hub.class.js";
 import { MovableObject } from "./movableObject.class.js";
 
 export class Cloud extends MovableObject{
@@ -9,6 +10,11 @@ export class Cloud extends MovableObject{
     constructor(){
         super().loadImage(ImageHelper.BACKGROUND.clouds[0]);
         this.x = Math.random() * 500;
+        this.animate();        
+    }
+
+    animate(){
+        IntervalHub.startInterval(() => this.x -= .15, 1000 / 60);
     }
 
 }
