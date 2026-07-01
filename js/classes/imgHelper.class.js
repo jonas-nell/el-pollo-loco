d5a612ecba68f47e349051140148a386c5ad438c
Jonas Nell <jonasnell1705@gmail.com>
2026-07-01 13:07:01 +0200

    feat: walking animation character

--- js/classes/imgHelper.class.js
+++ js/classes/imgHelper.class.js
@@ -13,7 +13,12 @@ export class ImageHelper {
             `assets/img/2_character_pepe/1_idle/idle/I-10.png`
         ],
         walk: [
-
+            `assets/img/2_character_pepe/2_walk/W-21.png`,
+            `assets/img/2_character_pepe/2_walk/W-22.png`,
+            `assets/img/2_character_pepe/2_walk/W-23.png`,
+            `assets/img/2_character_pepe/2_walk/W-24.png`,
+            `assets/img/2_character_pepe/2_walk/W-25.png`,
+            `assets/img/2_character_pepe/2_walk/W-26.png`
         ],
         jump: [
 
--- js/classes/movableObject.class.js
+++ js/classes/movableObject.class.js
@@ -1,22 +1,27 @@
-export class MovableObject{
+export class MovableObject {
     x = 120;
     y = 280;
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
 
-    moveRight(){
+    moveRight() {
         console.log("moving right");
-        
     }
 
-    moveLeft(){
-
-    }
+    moveLeft() {}
 }
