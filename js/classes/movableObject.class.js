bf25562792932d1d1cc7852c927626d9439ef443
Jonas Nell <jonasnell1705@gmail.com>
2026-07-07 15:35:42 +0200

    ref: added drawable-object class

--- js/classes/movableObject.class.js
+++ js/classes/movableObject.class.js
@@ -1,13 +1,8 @@
+import { DrawableObject } from "./drawable-object.class.js";
 import { IntervalHub } from "./interal-hub.class.js";
 
-export class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+export class MovableObject extends DrawableObject {
+
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -16,23 +11,6 @@ export class MovableObject {
     lastHit = 0;
 
     border = false;
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
 
     drawFrame(ctx) {
         ctx.beginPath();
