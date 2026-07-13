2a6c3f75cd55bba5d5908300f34f41b43df26566
Jonas Nell <jonasnell1705@gmail.com>
2026-07-13 19:28:52 +0200

    fix: enemy density

--- js/levels/level3.js
+++ js/levels/level3.js
@@ -23,7 +23,7 @@ export function level3(){
             //fourth segment
             new Chicken(SEGMENT_WIDTH, SEGMENT_WIDTH * 4), new Chicken(SEGMENT_WIDTH, SEGMENT_WIDTH * 4), new ChickenSmall(SEGMENT_WIDTH, SEGMENT_WIDTH * 4), new ChickenSmall(SEGMENT_WIDTH, SEGMENT_WIDTH * 4),
             //fifth segment
-            new Chicken(SEGMENT_WIDTH, SEGMENT_WIDTH * 5), new ChickenSmall(SEGMENT_WIDTH, SEGMENT_WIDTH * 5), new Chicken(SEGMENT_WIDTH, SEGMENT_WIDTH * 5),
+            new Chicken(SEGMENT_WIDTH, SEGMENT_WIDTH * 5), new ChickenSmall(SEGMENT_WIDTH, SEGMENT_WIDTH * 5), new Chicken(SEGMENT_WIDTH, SEGMENT_WIDTH * 5), new ChickenSmall(SEGMENT_WIDTH, SEGMENT_WIDTH * 5),
     
             endboss
         ],
