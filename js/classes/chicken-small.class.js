ba8fc1fc5ec3359962fdd0585ee85fff086329c7
Jonas Nell <jonasnell1705@gmail.com>
2026-07-15 12:50:23 +0200

    ref: cleaned up chicken-small

--- js/classes/chicken-small.class.js
+++ js/classes/chicken-small.class.js
@@ -2,7 +2,7 @@ import { Chicken } from "./chicken.class.js";
 import { ImageHelper } from "./img-helper.class.js";
 import { IntervalHub } from "./interval-hub.class.js";
 
-export class ChickenSmall extends Chicken{
+export class ChickenSmall extends Chicken {
     IMAGES_WALKING = ImageHelper.CHICKEN_SMALL.walk;
     IMAGES_DEAD = ImageHelper.CHICKEN_SMALL.dead;
 
@@ -10,15 +10,16 @@ export class ChickenSmall extends Chicken{
         top: 5,
         right: 5,
         bottom: 5,
-        left: 5
-    }
+        left: 5,
+    };
+
     y = 360;
     groundLevel = 360;
     height = 60;
     width = 60;
     speed = 0.2 + Math.random() * 0.4;
 
-    constructor(minX, maxX){
+    constructor(minX, maxX) {
         super(minX, maxX);
         this.loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
@@ -27,15 +28,14 @@ export class ChickenSmall extends Chicken{
         this.applyGravity();
     }
 
-    startJumping(){
+    startJumping() {
         const delay = 2000 + Math.random() * 2000;
 
         setTimeout(() => {
             if (!this.isDead()) {
-                this.speedY = 20 + Math.random() * 9; 
+                this.speedY = 20 + Math.random() * 9;
                 this.startJumping();
             }
         }, delay);
     }
-
 }
